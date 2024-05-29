9d570cbd1a161e5c7aaf57eb507af27b3093a369
Biiel25 <gabrielsantos@ucl.br>
2024-05-28 21:01:22 -0300

    teste commit

---
+++ Ronaldo Autos/Program.cs
@@ -0,0 +1,56 @@
+﻿/*Ronaldo Automoveis
+ * Cadastrar Veículos
+ * Consultar Estoque
+ * Registrar Venda
+ * Registrar Cliente
+ * Histórico de Vendas
+ */
+namespace Ronaldo_Autos
+{
+    internal class Program
+        class veiculo
+    {
+        private string marca, modelo;
+        private int ano, kilometragem;
+        private decimal preco;
+        public veiculo(string ma, string mo, int a, int km, decimal pre)
+        {
+            marca = ma;
+            modelo = mo;
+            ano = a;
+            kilometragem = km;
+            preco = pre;
+        }
+        public string getMarca()
+        {
+            return marca;
+        }
+        public string getModelo()
+        {
+            return modelo;
+        }
+        public int getAno()
+        {
+            return ano;
+        }
+        public int getKM()
+        {
+            return kilometragem;
+        }
+        public int getAno()
+        {
+            return ano;
+        }
+        public decimal getPreco()
+        {
+            return preco;
+        }
+    }
+
+            {
+        static void Main(string[] args)
+        {
+            veiculo corsa wind = new veiculo (string ma = ;
+        }
+    }
+}
